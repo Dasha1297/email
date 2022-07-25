5fa34e2578ec30266d89d8640b43ab90aa927674
Dasha Lukina <dashalukina1297@gmail.com>
2022-07-25 13:50:56 +0700

    Изменены запросы

--- server/routes/FolderRouter.js
+++ server/routes/FolderRouter.js
@@ -4,7 +4,7 @@ import FolderController from "../controllers/FolderController.js";
 const folderRouter = Router();
 folderRouter.get("/", FolderController.getFolders);
 folderRouter.post("/add", FolderController.addFolder);
-folderRouter.post("/update/:id", FolderController.updateFolder);
-folderRouter.post("/delete/:id", FolderController.deleteFolder);
+folderRouter.put("/update/:id", FolderController.updateFolder);
+folderRouter.delete("/delete/:id", FolderController.deleteFolder);
 
 export default folderRouter;
